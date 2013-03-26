42f254c6a84cd83b960040759c288549523d8b43
Diego Augusto de Faria Barros <diego@diego-IPMH61R2.(none)>
2013-03-26 20:05:32 -0300

    Primero Commit! Criação do Projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PortalBrazucas::Application.config.secret_token = '2b6d93afd15cc67ee32dec7511b104bc86364edeb317b88b7d981454ca38705d87bd15a15498778279d20b2b35a71f2da36b4fae4acef0f6f1fe0927b057f5db'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PortalBrazucas::Application.config.session_store :cookie_store, key: '_portal_brazucas_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PortalBrazucas::Application.config.session_store :active_record_store
