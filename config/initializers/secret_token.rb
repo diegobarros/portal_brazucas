267e056059531f1474ef791aee109d91cce4ead9
Diego Augusto de Faria Barros <diego@diego-IPMH61R2.(none)>
2013-03-26 23:16:24 -0300

    Configuracao do Ambiente

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PortalBrazucas::Application.config.secret_token = '2b6d93afd15cc67ee32dec7511b104bc86364edeb317b88b7d981454ca38705d87bd15a15498778279d20b2b35a71f2da36b4fae4acef0f6f1fe0927b057f5db'
+PortalBrazucas::Application.config.secret_token = '4662f1ea9af51a6e1d61426452afcba8360a784e22e78f04b633f7a1626c144695c870bf3137109a6dc96a2c8c5979990fd1b1c1e0261da1a0748a4f47367713'
